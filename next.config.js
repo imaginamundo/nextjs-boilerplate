d00154f899db754288f3881ed38837ecaa4ce809
Diogo Ferreira Fernandes <dio@mac.local>
2020-02-05 19:05:03 -0300

    Remove export

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 const withPlugins = require('next-compose-plugins');
 
 const nextConfig = {
-    poweredByHeader: false,
-    exportTrailingSlash: true,
-    exportPathMap: function() {
-        return {
-            '/': { page: '/' },
-            '/how-it-works': { page: '/how-it-works' },
-            '/dependencies': { page: '/dependencies' },
-            '/releases': { page: '/releases' },
-            '/contributors': { page: '/contributors' }
-        };
-    }
+    poweredByHeader: false
 };
 
 module.exports = withPlugins([ ], nextConfig);
