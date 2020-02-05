64ac50b95665b4212103657896f97ab05c3cf98f
Diogo Ferreira Fernandes <dio@mac.local>
2020-02-05 19:02:43 -0300

    Add export static html

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 const withPlugins = require('next-compose-plugins');
 
 const nextConfig = {
-    poweredByHeader: false
+    poweredByHeader: false,
+    exportTrailingSlash: true,
+    exportPathMap: function() {
+        return {
+            '/': { page: '/' },
+            '/how-it-works': { page: '/how-it-works' },
+            '/dependencies': { page: '/dependencies' },
+            '/releases': { page: '/releases' },
+            '/contributors': { page: '/contributors' }
+        };
+    }
 };
 
 module.exports = withPlugins([ ], nextConfig);
