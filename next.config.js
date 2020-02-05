04d6832e01009722acec8ee2c0001f03f5df1418
Diogo Ferreira Fernandes <dio@mac.local>
2020-02-05 19:36:41 -0300

    Add pwa support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,42 @@
 const withPlugins = require('next-compose-plugins');
+const withOffline = require('next-offline')
 
 const nextConfig = {
     poweredByHeader: false
 };
 
-module.exports = withPlugins([ ], nextConfig);
+module.exports = withPlugins([
+    [
+        withOffline,
+        {
+            workboxOpts: {
+                swDest: process.env.NEXT_EXPORT
+                    ? 'service-worker.js'
+                    : 'static/service-worker.js',
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https?.*/,
+                        handler: 'NetworkFirst',
+                        options: {
+                            cacheName: 'offlineCache',
+                            expiration: {
+                                maxEntries: 200,
+                            },
+                        },
+                    },
+                ],
+            },
+            experimental: {
+                async rewrites() {
+                    return [
+                        {
+                            source: '/service-worker.js',
+                            destination: '/_next/static/service-worker.js',
+                        },
+                    ]
+                },
+            }
+        }
+    ]
+], nextConfig);
 
